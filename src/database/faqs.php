159045d2d684811dfdb947b87ae74df1ac34e145
Ntsay Zacarias <ntsayyzacarias@gmail.com>
2023-04-28 16:38:15 +0100

    changes

--- src/database/faqs.php
+++ src/database/faqs.php
@@ -12,5 +12,23 @@ function get_faqs(){
     }
 }
 
+function submit_faq($question, $answer){
+    global $dbh;
+    try {
+  	  $stmt = $dbh->prepare('INSERT INTO faqs ()
+       VALUES ()');
+  	  $stmt->bindParam(':username', $username);
+      if($stmt->execute()){
+        $id = get_id($username);
+        return $id;
+      }
+      else
+        return -1;
+    }catch(PDOException $e) {
+      
+      return -1;
+    }
+}
+
 
 ?>
